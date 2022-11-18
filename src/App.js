1c1e580e64184e06f19fb6f481bff3f5db7db294
Dawid Popiolek <dawidpopiolek@protonmail.com>
2022-11-18 15:33:01 +0100

    App can now delete tasks and updates the checkbox

--- src/App.js
+++ src/App.js
@@ -1,20 +1,50 @@
+import React, { useState } from "react";
+import { nanoid } from "nanoid";
+
+
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
 function App(props) {
-  const taskList = props.tasks.map((task) => (
+  const [tasks, setTasks] = useState(props.tasks);
+  
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+  
+  
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      if (id === task.id) {
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+  
+  const taskList = tasks.map((task) => (
       <Todo 
-        id={task.id} 
+        id={task.id}
         name={task.name} 
         completed={task.completed}
         key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
       />
     )
   );
+
   function addTask(name) {
-    alert(name);
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    setTasks([...tasks, newTask])
   }
+
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -24,9 +54,7 @@ function App(props) {
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">
-        3 tasks remaining
-      </h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
