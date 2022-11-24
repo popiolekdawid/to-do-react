da3a2e25ba302de6f74f49d948d6d3c077f71ce2
Dawid Popiolek <dawidpopiolek@protonmail.com>
2022-11-24 13:21:34 +0100

    added editing mode to tasks, filter buttons work

--- src/App.js
+++ src/App.js
@@ -6,8 +6,17 @@ import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('All');
+
   
   function deleteTask(id) {
     const remainingTasks = tasks.filter((task) => id !== task.id);
@@ -34,10 +43,12 @@ function App(props) {
     setTasks(updatedTasks);
   }
   
-  const taskList = tasks.map((task) => (
-    <Todo 
+  const taskList = tasks
+  .filter(FILTER_MAP[filter])
+  .map((task) => (
+    <Todo
       id={task.id}
-      name={task.name} 
+      name={task.name}
       completed={task.completed}
       key={task.id}
       toggleTaskCompleted={toggleTaskCompleted}
@@ -45,6 +56,16 @@ function App(props) {
       editTask={editTask}
     />
   ));
+  
+
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));  
 
   function addTask(name) {
     const newTask = { id: `todo-${nanoid()}`, name, completed: false };
@@ -59,9 +80,7 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
